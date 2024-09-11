7acd7e02b2d3f13683616af095b529d7ff91847a
retbleed <hsa.lapaz@gmail.com>
2024-09-10 17:00:25 -0700

    Threads

---
+++ src/threads/ThreadMaker.java
@@ -0,0 +1,33 @@
+package threads;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreadMaker extends Thread {
+    private final long minTime;
+    private final List<Character> availableLetters;
+    private static final List<Character> usedLetters = Collections.synchronizedList(new ArrayList<>());
+
+    public ThreadMaker(long minTime, String name) {
+        this.minTime = minTime;
+        this.availableLetters = new ArrayList<>();
+        for (char c : name.toCharArray()) {
+            this.availableLetters.add(c);
+        }
+    }
+
+    public void run() {
+        while (!availableLetters.isEmpty()) {
+            try {
+                Collections.shuffle(availableLetters);
+                char selectedChar = availableLetters.removeFirst();
+                System.out.print(selectedChar + " ");
+                usedLetters.add(selectedChar);
+                Thread.sleep(minTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/threads/ThreadWrapper.java
@@ -0,0 +1,29 @@
+package threads;
+
+import java.util.Random;
+
+public class ThreadWrapper {
+    private final String fullName;
+
+    public ThreadWrapper(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void run() {
+        String[] nameParts = this.fullName.split(" ");
+        Random random = new Random();
+        long initialMinTime;
+
+        System.out.print(fullName + "\n");
+        for (int i = 0; i < nameParts.length; i++) {
+            initialMinTime = Math.abs(random.nextLong() % 100);
+            ThreadMaker thread = new ThreadMaker(initialMinTime + (i * 50L), nameParts[i]);
+            thread.start();
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadWrapper wrapper = new ThreadWrapper("John Doe");
+        wrapper.run();
+    }
+}
